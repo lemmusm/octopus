564db3ff349ed8db8efea28e3f46326aa3db18e3
lemmusm <lemmusm@gmail.com>
2020-03-06 14:17:51 -0600

    Update content for contact page

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,10 @@ import { useStaticQuery, graphql } from "gatsby"
 import Code from "../assets/svg/code.svg"
 import Love from "../assets/svg/heart.svg"
 import GatsbyLogo from "../assets/svg/logogatsby.svg"
+import IconGithub from "../assets/svg/github.svg"
+import IconMedium from "../assets/svg/medium.svg"
+import IconLinkedin from "../assets/svg/linkedin.svg"
+import IconTwitter from "../assets/svg/twitter.svg"
 
 import Header from "./header"
 import "./layout.css"
@@ -33,23 +37,57 @@ const Layout = ({ children }) => {
       <div className="flex flex-wrap px-1 mt-32 sm:mt-40 md:mt-40 lg:mt-20 lg:px-12 xl:px-32">
         <main className="flex w-full">{children}</main>
         <footer className="flex w-full inline-block text-xs font-light justify-center items-center my-5">
-          <Code className="inline-block mr-1" /> with
-          <Love className="heart inline-block ml-1 mr-1" />{" "}
-          <a
-            href="https://twitter.com/lemmusm"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            by Manuel Hdez Lemus
-          </a>
-          ©{new Date().getFullYear()}, built with{" "}
-          <a
-            href="https://www.gatsbyjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <GatsbyLogo className="inline-block ml-1 mr-1" />
-          </a>
+          <div className="w-full flex flex-wrap text-center">
+            <div className="w-full">
+              <Code className="inline-block mr-1" /> with
+              <Love className="heart inline-block ml-1 mr-1" />{" "}
+              <a
+                href="https://twitter.com/lemmusm"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                by Manuel Hdez Lemus
+              </a>
+              ©{new Date().getFullYear()}, built with{" "}
+              <a
+                href="https://www.gatsbyjs.org/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <GatsbyLogo className="inline-block ml-1 mr-1" />
+              </a>
+            </div>
+            <div className="w-full">
+              <a
+                href="https://www.linkedin.com/in/lemmusm/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <IconLinkedin className="inline-block mr-2 linkContactFooter" />
+              </a>
+              <a
+                href="https://github.com/lemmusm"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <IconGithub className="inline-block ml-2 mr-2 linkContactFooter" />
+              </a>
+              <a
+                href="https://twitter.com/lemmusm"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <IconTwitter className="inline-block ml-2 mr-2 linkContactFooter" />
+              </a>
+              <a
+                href="https://medium.com/@lemmusm"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <IconMedium className="inline-block ml-2 mr-2 linkContactFooter" />
+              </a>
+            </div>
+          </div>
         </footer>
       </div>
     </>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,11 +2,76 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import IconGithub from "../assets/svg/github.svg"
+import IconMedium from "../assets/svg/medium.svg"
+import IconLinkedin from "../assets/svg/linkedin.svg"
+import IconTwitter from "../assets/svg/twitter.svg"
 
 const SecondPage = () => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from contact page</h1>
+    <SEO title="Contact" />
+    <div className="lg:mt-20 pt-20 xl:0 p-5 w-full flex flex-wrap">
+      <div className="w-1/4 py-5">
+        <div className="w-full flex px-6 py-4 justify-center">
+          <a
+            href="https://github.com/lemmusm"
+            className="linkContact"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <IconGithub />
+          </a>
+        </div>
+      </div>
+      <div className="w-1/4 py-5">
+        <div className="w-full flex px-6 py-4 justify-center">
+          <a
+            href="https://www.linkedin.com/in/lemmusm/"
+            className="linkContact"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <IconLinkedin />
+          </a>
+        </div>
+      </div>
+      <div className="w-1/4 py-5">
+        <div className="w-full flex px-6 py-4 justify-center">
+          <a
+            href="https://twitter.com/lemmusm"
+            className="linkContact"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <IconTwitter />
+          </a>
+        </div>
+      </div>
+      <div className="w-1/4 py-5">
+        <div className="w-full flex px-6 py-4 justify-center">
+          <a
+            href="https://medium.com/@lemmusm"
+            className="linkContact"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <IconMedium />
+          </a>
+        </div>
+      </div>
+      <div className="w-full py-2">
+        <div className="w-full flex justify-center">
+          <a
+            href="https://github.com/lemmusm"
+            rel="noopener noreferrer"
+            className="gradientText"
+            target="_blank"
+          >
+            lemmusm@gmail.com
+          </a>
+        </div>
+      </div>
+    </div>
   </Layout>
 )
 
